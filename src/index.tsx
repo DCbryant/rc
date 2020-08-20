67f77073645e92c130a758e426b39b8a9aad5d41
dcbryant <cong.ding@100credit.com>
2020-08-20 16:41:23 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export {default as Button} from "./components/button";
+export {default as GlobalStyle} from "./components/shared/global";
+export * from "./components/shared/styles";
+export * from "./components/shared/animation";
