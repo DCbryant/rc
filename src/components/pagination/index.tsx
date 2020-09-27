a3ed8903340650fe2ab821d7c7f49ae5018f8d72
dcbryant <cong.ding@100credit.com>
2020-09-27 18:27:12 +0800

    feat(page): add commit

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -36,6 +36,7 @@ const PageUl = styled.ul`
 	}
 `;
 
+// 动态生成一个数组
 function calculateMove(
 	current: number,
 	state: number[],
@@ -43,22 +44,27 @@ function calculateMove(
 ): number[] | null {
 	let mid = Math.floor(state.length / 2);
 	let arr;
+	// 当前位置跟中间数的差距
 	let minus = current - state[mid];
+	// 当当前位置跟中间数相等的时候无需更新
 	if (minus === 0) {
 		arr = null;
-	} else if (minus > 0) {
+	} else if (minus > 0) { // 当前位置大于中间数也就是点击右边的时候
+		// 最后一个数
 		let tmp = state[state.length - 1];
+		// 群体加差值
 		if (tmp + minus < totalPage) {
 			arr = state.map((v) => v + minus);
 		} else {
 			if (tmp === totalPage) {
 				arr = null;
-			} else {
+			} else { // tmp + minus > totalPage 此时重新生成一个差值，totalPage - tmp，之前的差值超过总数了
 				arr = state.map((v) => v + totalPage - tmp);
 			}
 		}
-	} else {
+	} else { //当前位置小于中间数也就是点击左边的时候
 		//负数
+		// 第一个元素加差值 > 1就可以正常左移
 		if (state[0] + minus > 1) {
 			arr = state.map((v) => v + minus);
 		} else {
@@ -103,7 +109,9 @@ export function Pagination(props: PaginationProps) {
 	const [current, setCurrent] = useState(defaultCurrent!);
 	const [state, setState] = useState<Array<number>>([]);
 	const totalPage = useMemo(() => {
+		// 99 / 10
 		let number = Math.ceil(total / pageSize!);
+		// 如果分页数大于最大分页数
 		if (number > barMaxSize!) {
 			let statetmp = new Array(barMaxSize).fill(1).map((_x, y) => y + 1);
 			setState(statetmp);
@@ -144,7 +152,7 @@ export function Pagination(props: PaginationProps) {
 								if (arr) {
 									setState(arr);
 								}
-							} else if (current !== state[0]) {
+							} else if (current !== state[0]) { //state[0] = 1,即是current不为1
 								setCurrent(current - 1);
 								let arr = calculateMove(
 									current - 1,
